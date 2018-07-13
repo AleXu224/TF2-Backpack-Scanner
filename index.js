d8d2d205d203330929f59efae8451d00bfb96bb0
Alexandru Iustin <iustin224@gmail.com>
2018-07-13 15:18:09 +0300

    First window

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var {app, BrowserInfo} = require('electron');
+var {app, BrowserWindow} = require('electron');
 
+app.on("ready", () => {
+	var win = new BrowserWindow({minWidth:800,minHeight:600})
+	win.loadURL(`file:///${__dirname}/index.html`);
+})
