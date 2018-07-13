6d8186b4cac54821626c28e48a1d13bfb857fd1d
Alexandru Iustin <iustin224@gmail.com>
2018-07-13 17:08:21 +0300

    Added a basic layout with a functional sidebar

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var {app, BrowserWindow} = require('electron');
 app.on("ready", () => {
 	var win = new BrowserWindow({minWidth:800,minHeight:600})
 	win.loadURL(`file:///${__dirname}/index.html`);
+	win.setMenu(null);
 })
