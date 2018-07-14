48bbe36c6175a88c377717907e31073c16fa68a3
Alexandru Iustin <iustin224@gmail.com>
2018-07-14 15:47:56 +0300

    Updated the interface and added a settings menu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var {app, BrowserWindow} = require('electron');
 app.on("ready", () => {
 	var win = new BrowserWindow({minWidth:800,minHeight:600,frame:false})
 	win.loadURL(`file:///${__dirname}/index.html`);
-	win.setMenu(null);
+	// win.setMenu(null);
 })
 
 app.on("window-all-closed", () => {
--- menuhandler.js
+++ menuhandler.js
@@ -1,4 +1,13 @@
 var { remote } = require('electron');
+try {
+	var config = require('./config.json');
+	
+	if (config.apikey != undefined) {
+		document.getElementById("apikey").value = config.apikey;
+	}
+} catch (error) {
+	console.log(`Config has not been created yet`);
+}
 
 var win = remote.getCurrentWindow();
 
@@ -16,4 +25,25 @@ function minimize() {
 
 function close_window() {
 	win.close();
+}
+
+function saveSettings() {
+	var apikey = document.getElementById("apikey").value;
+	if (apikey.length != 32) {
+		document.getElementById("apikey").parentNode.children[0].innerHTML = "Error: invalid key"
+	} else {
+		var config = {
+			"apikey": apikey
+		}
+		require('fs').writeFile('./config.json', JSON.stringify(config), (err) => {
+			if (err){
+				console.log(err);
+			}
+		})
+	}
+}
+
+function clearError(i) {
+	var option = document.getElementsByClassName("option")[i];
+	option.children[0].innerHTML = "Api Key"
 }
