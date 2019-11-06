be77bf2e24ba4ec3c60353b760b1585075aa0ce9
AleXu224 <iustin224@gmail.com>
2019-11-06 17:34:13 +0200

    1.2.7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const debug = require('electron-debug');
 debug({showDevTools:false});
 
 app.on("ready", () => {
-	var win = new BrowserWindow({minWidth:800,minHeight:600,frame:false})
+	var win = new BrowserWindow({ minWidth: 800, minHeight: 600, frame: false, webPreferences: { nodeIntegration: true } });
 	win.loadURL(`file:///${__dirname}/index.html`);
 	// win.webContents.openDevTools()
 	// win.setMenu(null);
